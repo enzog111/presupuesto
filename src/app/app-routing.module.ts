86fc5301f9b026e5296cc3c9d35840f7c78e56e0
enzo <enzomaza@gmail.com>
2022-02-10 09:51:13 -0300

    se creó los componentes: Componentes, y Servicios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GastosComponent } from './components/gastos/gastos.component';
 
-const routes: Routes = [];
+  //Componentes
+  import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+  import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+
+  const routes: Routes = [
+    {path:'', redirectTo:'/IngresarPresupuesto', pathMatch: 'full'},
+    {path:'IngresarPresupuesto',component:IngresarGastoComponent},
+    {path:'gasto',component:GastosComponent},
+    {path:'**',redirectTo:'/IngresarPresupuesto', pathMatch: 'full'}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
